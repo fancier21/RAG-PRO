0bf91d9438821b04d6174236d1ee33f0e589f3d9
fancier21 <fancier@proton.me>
2026-05-28 13:36:28 +0400

    langchain llm config

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
+import getpass
+import os
+
+from dotenv import load_dotenv
+from langchain_openai import ChatOpenAI
+
+load_dotenv()
+
+if not os.environ.get("OPENAI_API_KEY"):
+    os.environ["OPENAI_API_KEY"] = getpass.getpass("Enter your OpenAI API key: ")
+
+
 def main():
-    print("Hello from rag-pro!")
+    llm = ChatOpenAI(model="gpt-5.4-mini")
+
+    first_query = "Hi, I'm Bob."
+    messages = [{"role": "user", "content": first_query}]
+
+    response = llm.invoke(messages)
+    print(f"Response: {response.text}")
 
 
 if __name__ == "__main__":
